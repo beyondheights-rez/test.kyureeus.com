f7ea1493b83c95e7d259e20d7c7e18461362c34d
Gokul  Ayyasamy <rezilyens.com@gmail.com>
2026-09-02 16:14:22 +0530

    empty

--- src/lib/utils/cn.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/**
- * Combines multiple Tailwind CSS classes and resolves conflicts using tailwind-merge.
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
